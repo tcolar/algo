5a032f649c6eb5fbc9035495c40431e648f1b246
Thibaut Colar <tcolar@colar.net>
2013-12-01 21:33:31 -0800

    go mutex test

---
+++ mutex_test.go
@@ -0,0 +1,66 @@
+// History: Dec 01 13 tcolar Creation
+
+package algo
+
+import (
+	"log"
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T) {
+
+	var mutex = sync.Mutex{}
+	// this will be incremented using mutex
+	var total = struct {
+		Total int64
+	}{Total: 7}
+	// this will be updated using atomic.AddInt64
+	var total2 int64 = 7
+
+	// Start a hundred routines
+	for i := 0; i < 50; i++ {
+		go func() {
+			// Each updating the total values a 100 times
+			for j := 0; j != 50; j++ {
+				toAdd := int64(i)
+				// adding or removing  'j' to total if eve or odd
+				if j%2 == 0 {
+					toAdd *= -1
+				}
+				// using the mutex
+				mutex.Lock()
+
+				// Doing the addition in such a way that it makes it much more likely
+				// that their would be a race condition (when not using a mutex)
+				// Not so easy to make happen somehow.
+				t := total.Total
+				time.Sleep(time.Duration(rand.Int()%1000) * time.Microsecond)
+				t += toAdd
+				total.Total = t
+
+				mutex.Unlock()
+
+				// using atomic
+				atomic.AddInt64(&total2, toAdd)
+
+				// Keep scheduler happy
+				runtime.Gosched()
+			}
+		}()
+	}
+
+	// Give some time to compute
+	time.Sleep(5 * time.Second)
+
+	// Both totals should still have the start value by the time it ends
+	if total.Total != 7 || total2 != 7 {
+		log.Printf("Total: %d", total)
+		log.Printf("Total2: %d", total2)
+		log.Panic("Unexpected values")
+	}
+}
