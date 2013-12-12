3413904980ca2c90014f8d2dd970cd4472469364
Thibaut Colar <tcolar@colar.net>
2013-12-12 09:32:53 -0800

    Binary trees

--- tree_test.go
+++ tree_test.go
@@ -13,9 +13,26 @@ import (
 //
 // Def: For every node,the node to the left of it has a less value(key) and the
 // node to the right of it has a greater value(key)
-func IsBinary(tree *Tree) {
-	// go down from root, if NOT (left <= x > right) fail ?
+func IsBinary(tree *TreeNode) bool {
+	// recurse down from root, if NOT (left <= x > right) fail ?
 	// otherwise recurse to left, right
+	if tree.Left != nil {
+		if tree.Left.Val > tree.Val {
+			return false
+		}
+		if !IsBinary(tree.Left) {
+			return false
+		}
+	}
+	if tree.Right != nil {
+		if tree.Right.Val <= tree.Val {
+			return false
+		}
+		if !IsBinary(tree.Right) {
+			return false
+		}
+	}
+	return true
 }
 
 func TestTree(t *testing.T) {
@@ -24,5 +41,17 @@ func TestTree(t *testing.T) {
 	tree := Tree{
 		root: BuildTestBinSearchTree(0, 10),
 	}
-	log.Print(TreeDump(tree.root, ""))
+	if !IsBinary(tree.root) {
+		log.Print(TreeDump(tree.root, ""))
+		log.Fatal("Tree should be a binary search tree")
+	}
+	node := tree.root
+	for node.Right != nil {
+		node = node.Right
+	}
+	node.Val = 0
+	if IsBinary(tree.root) {
+		log.Print(TreeDump(tree.root, ""))
+		log.Fatal("Tree should *NOT* be a binary search tree")
+	}
 }
